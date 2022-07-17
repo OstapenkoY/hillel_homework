4c73c6b7ee4159315efb1fefa0885d915dddf9e2
Yurii Ostapenko <aweeery@gmail.com>
2022-07-17 16:04:01 +0300

    Added HW4 folder, modified main.py, utils.py

--- HW4/main.py
+++ HW4/main.py
@@ -1,202 +1,61 @@
-import datetime
-import pprint
-import random
-import string
-
-import requests
-from flask import Flask, request, jsonify, Response
-
-from http import HTTPStatus
-
+from flask import Flask
 from database_handler import execute_query
-
 from webargs import validate, fields
 from webargs.flaskparser import use_kwargs
+from utils import format_records, track_info_format_records
+import sqlite3
 
-from utils import format_records, my_format_records
+
+def execute_query(query, args=()):
+    with sqlite3.connect('chinook.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute(query, args)
+        connection.commit()
+        records = cursor.fetchall()
+    return records
 
 app = Flask(__name__)
-
-
-@app.errorhandler(HTTPStatus.UNPROCESSABLE_ENTITY)
-@app.errorhandler(HTTPStatus.BAD_REQUEST)
-def error_handling(error):
-    headers = error.data.get("headers", None)
-    messages = error.data.get("messages", ["Invalid request."])
-
-    if headers:
-        return jsonify(
-            {
-                'errors': messages
-            },
-            error.code,
-            headers
-        )
-    else:
-        return jsonify(
-            {
-                'errors': messages
-            },
-            error.code,
-        )
-
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-@app.route("/hello")
-def hello():
-    print('testd')
-    return "<p>Hello, Mykhailo!</p>"
-
-
-@app.route("/now")
-def get_current_time():
-    return f"{datetime.datetime.now()}"
-
-
-@app.route("/generate-password")
-@use_kwargs(
-    {
-        "length": fields.Int(
-            missing=10,
-            validate=[validate.Range(min=8, max=100)]
-        ),
-    },
-    location="query"
-)
-def generate_password(length):
-    return "".join(
-        random.choices(
-            string.ascii_lowercase + string.ascii_uppercase,
-            k=length
-        )
-    )
-
-
-@app.route("/get-astronauts")
-def get_astronauts():
-    url = "http://api.open-notify.org/astros.json"
-    result = requests.get(url, {})
-
-    if result.status_code not in (HTTPStatus.OK,):
-        return Response("ERROR: Something went wrong",
-                        status=result.status_code)
-
-    result = result.json()
-    statistics = {}
-    for entry in result.get('people', {}):
-        statistics[entry['craft']] = statistics.get(entry['craft'], 0) + 1
-
-    pprint.pprint(statistics)
-
-    return statistics
-
-
-@app.route("/customers")
-@use_kwargs(
-    {
-        "first_name": fields.Str(
-            required=False,
-            missing=None,
-            validate=[validate.Regexp("^[0-9]*")]
-        ),
-        "last_name": fields.Str(
-            required=False,
-            missing=None,
-            validate=[validate.Regexp("^[0-9]*")]
-        )
-    },
-    location="query"
-)
-def get_all_customers(first_name, last_name):
-    query = "SELECT * FROM customers"
-
-    fields = {}
-
-    if first_name:
-        fields["FirstName"] = first_name
-
-    if last_name:
-        fields["LastName"] = last_name
-
-    if fields:
-        query += " WHERE " + " AND ".join(
-            f"{key}=?" for key in fields.keys()
-        )
-
-    records = execute_query(query=query, args=tuple(fields.values()))
-
-    return format_records(records)
-
 
 @app.route("/order-price")
 @use_kwargs(
     {
-        "country": fields.Str(
-            required=False,
-            missing=None,
-            validate=[validate.Regexp("^[0-9]*")]
-        )
+        "country": fields.Str(required=False,
+                              load_default=None)
     },
     location="query"
 )
 def order_price(country):
-    query = "SELECT invoices.BillingCountry, round(sum(invoice_items.UnitPrice), 2)  \
-                FROM invoice_items \
-                JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId "
-    fields = {}
-
+    query = """SELECT invoices.BillingCountry, round(sum(invoice_items.UnitPrice), 2)
+               FROM invoice_items 
+               JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId"""
     if country:
-        fields['invoices.BillingCountry'] = country
-
-    if fields:
-        query += " AND " + ''.join(f"{key}=?" for key in fields.keys()) + " GROUP BY invoices.BillingCountry;"
-
-        records = execute_query(query=query, args=tuple(fields.values()))
-        return format_records(records)
+        query += " AND " + f"invoices.BillingCountry='{country}'" + " GROUP BY invoices.BillingCountry;"
     else:
         query += " GROUP BY invoices.BillingCountry;"
-
-        records = execute_query(query=query, args=tuple(fields.values()))
-        return format_records(records)
+    records = execute_query(query)
+    return format_records(records)
 
 
 @app.route("/track-info")
 @use_kwargs(
     {
-        "id": fields.Str(
-            required=False,
-            missing=None,
-            validate=[validate.Regexp("^[a-zA-Z]*")]
-        )
+        "id": fields.Int(required=False,
+                         load_default=None)
     },
     location="query"
 )
 def get_all_info_about_track(id):
-    query = "SELECT tracks.TrackID, tracks.Composer, tracks.Name AS TrackName, tracks.UnitPrice, \
-                    tracks.Milliseconds, tracks.Bytes, albums.Title AS AlbumTitle, artists.Name AS ArtistName, \
-                    genres.Name AS Genre, media_types.Name AS MediaType \
-            FROM tracks JOIN albums, artists, genres, media_types \
-                WHERE albums.AlbumId = tracks.AlbumId \
-                    AND albums.ArtistId = artists.ArtistId AND tracks.GenreId = genres.GenreId \
-                    AND tracks.MediaTypeId = media_types.MediaTypeId"
-    fields = {}
+    query = """SELECT tracks.TrackID, tracks.Composer, tracks.Name AS TrackName, tracks.UnitPrice, 
+               tracks.Milliseconds, tracks.Bytes, albums.Title AS AlbumTitle, artists.Name AS ArtistName, 
+               genres.Name AS Genre, media_types.Name AS MediaType 
+               FROM tracks JOIN albums, artists, genres, media_types 
+               WHERE albums.AlbumId = tracks.AlbumId 
+               AND albums.ArtistId = artists.ArtistId AND tracks.GenreId = genres.GenreId 
+               AND tracks.MediaTypeId = media_types.MediaTypeId"""
+    query += " AND " + f"tracks.TrackID='{id}'"
+    records = execute_query(query=query)
+    return track_info_format_records(records)
 
-    if id:
-        try:
-            int(id)
-            fields['tracks.TrackID'] = id
-        except ValueError:
-            return '<h3>Id must be numeric</h3>'
-    else:
-        return '<h3>Enter track id</h3>'
-
-    query += " AND " + ''.join(f"{key}=?" for key in fields.keys())
-    records = execute_query(query=query, args=tuple(fields.values()))
-    return my_format_records(records)
 
 @app.route("/total-time")
 def get_total_track_time():
--- HW4/utils.py
+++ HW4/utils.py
@@ -2,13 +2,12 @@ def format_records(records: list) -> str:
     return '<br>'.join(str(record) for record in records)
 
 
-def my_format_records(records: list) -> str:
+def track_info_format_records(records: list) -> str:
     title_names = ['TrackId', 'Composer', 'TrackName', 'UnitPrice',
                    'Milliseconds', 'Bytes', 'AlbumTitle',
                    'ArtistName', 'Genre', 'MediaType']
-    d = {}
-    for i in range(10):
-        d[title_names[i]] = records[0][i]
-    return str({f'<br>{k}: {v}' for k, v in d.items()})
+    total = list(zip(title_names, records[0]))
+    return '<br>'.join(str(record) for record in total)
+
 
 
